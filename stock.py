1638c7187d52c80d299a196a717a998715477e17
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-08 04:55:51 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -32,9 +32,9 @@ tickers=('TSLA','AAPL','MSFT','BTC-USD','ETH-USD','AMD','AMZN')
 dropdown1=st.multiselect('Pick your assets',tickers,key=2)
 start=st.date_input('Start',value =pd.to_datetime('2022-07-12'))
 end=st.date_input('End',value=pd.to_datetime('2022-09-08'))
+def __init__(tickers, name='TSLA'):
 dataset = yf.download(dropdown1,start,end)['Adj Close']
 st.title('Weekly Stock Adj Close for Monday')
 weekly_Monday = dataset.asfreq('W-Mon')
 fig, ax = plt.subplots(figsize=(16, 4))
 st.line_chart(dataset)
-def __init__(tickers, name='TSLA'):
