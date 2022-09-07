1972cf2b873790a9a89a18b355dce70b55b12f14
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-08 04:53:02 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -32,6 +32,7 @@ tickers=('TSLA','AAPL','MSFT','BTC-USD','ETH-USD','AMD','AMZN')
 dropdown1=st.multiselect('Pick your assets',tickers,key=2)
 start=st.date_input('Start',value =pd.to_datetime('2022-07-12'))
 end=st.date_input('End',value=pd.to_datetime('2022-09-08'))
+def __init__(tickers, name='TSLA'):
 st.subheader('Please select a stock to rectify the error')
 dataset = yf.download(dropdown1,start,end)['Adj Close']
 st.title('Weekly Stock Adj Close for Monday')
