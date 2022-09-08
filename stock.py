358336af38a0eb1a0102a2de04243710b83ef224
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-08 14:47:11 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -63,8 +63,6 @@ dropdown=st.multiselect('Pick your assets',tickers,key=5,default='TSLA')
 start = dt.date.today() - dt.timedelta(days = 365*5)
 end = dt.date.today()
 dataset= yf.download(dropdown,start,end)['Adj Close']
-dataset_series=pd.Series(tuple(dataset))
-dataset=dataset_series.get(tickers,0)
 Start = 5000
 dataset['Shares'] = 0
 dataset['PnL'] = 0
