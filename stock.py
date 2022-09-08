62ad3607e412815d78b9c97dbb24eeb79ec7da7c
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-09 03:18:31 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -183,4 +183,7 @@ dataset['Returns'] = np.log(dataset['Adj Close'] / dataset['Adj Close'].shift(1)
 dataset = dataset.dropna()
 X = dataset['Open']
 Y = dataset['Adj Close']
-st.map(X,Y)
+st.text('Open')
+st.line_chart(X)
+st.text('Adj Close')
+st.line_chart(Y)
