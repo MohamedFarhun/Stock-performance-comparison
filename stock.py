47754b1da42c3690ebddedef07a75c2709775d76
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-08 12:08:58 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -29,7 +29,7 @@ if len(dropdown)>0:
     
 st.title('Stock time series analysis')
 tickers=('TSLA','AAPL','MSFT','BTC-USD','ETH-USD','AMD','AMZN')
-dropdown=st.multiselect('Pick your assets',tickers,key=2)
+dropdown=st.multiselect('Pick your assets',tickers,key=2,default='TSLA')
 start=st.date_input('Start',value =pd.to_datetime('2022-07-12'))
 end=st.date_input('End',value=pd.to_datetime('2022-08-05'))
 st.subheader('Please add a stock to rectify the error')
