103cae016e0299eebb5da9c7b3bde9798580eae9
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-09 00:25:11 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -143,4 +143,5 @@ for ticker in tickers:
 rets = df.pct_change()
 rets1=rets.columns
 rets2=rets.std()
-st.bar_chart(rets1, rets2, color=['red', 'blue', 'green', 'orange', 'cyan'])
+rets3=rets.color=['red', 'blue', 'green', 'orange', 'cyan']
+st.bar_chart(rets1, rets2, rets3)
