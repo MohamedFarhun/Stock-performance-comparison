75ec4e104273a005ee29f5965c74ff0444073c8e
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-08 17:50:10 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -90,7 +90,7 @@ y_train=y_train.reshape(-1,1)
 lr = LinearRegression()
 fit=lr.fit(x_train, y_train)
 st.text(fit)
-x_test = x_test[:-1]
-y_test = y_test[1:]
+x_test = x_test[2:-1]
+y_test = y_test[0:2]
 score=lr.score(x_test, y_test)
 st.text(score)
