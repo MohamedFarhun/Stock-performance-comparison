fe929139897666255dd93fec987b8e3b1a7fcb5c
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-08 17:18:17 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -84,6 +84,7 @@ Y_train = df['Adj Close']
 X_train = X_train.values[:-1]
 Y_train = Y_train.values[1:]
 lr = LinearRegression()
+X_train=X_train.reshape(-1,1)
 Y_train=Y_train.reshape(-1,1)
 lr.fit(X_train, Y_train)
 lr.score(X_test, Y_test)
