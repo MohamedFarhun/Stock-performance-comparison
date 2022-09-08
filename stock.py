1f6fa6766fb0a4c5f9a53583421308366f88d2d5
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-08 18:09:36 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -90,7 +90,7 @@ y_train=y_train.reshape(-1,1)
 lr = LinearRegression()
 fit=lr.fit(x_train, y_train)
 st.text(fit)
-x_test=df.iloc[:,:2]
+x_test=df.iloc[:,:1]
 y_test=df['Adj Close']
 score=lr.score(x_test, y_test)
 st.text(score)
