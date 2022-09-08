25066960d69eee5d3ae786070b4dbe62807c1518
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-08 12:00:42 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -3,6 +3,18 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 
+with st.form("my_form"):
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Outside the form")
+
 st.title('Stock performance comparison')
 
 tickers=('TSLA','AAPL','MSFT','BTC-USD','ETH-USD','AMD','AMZN')
