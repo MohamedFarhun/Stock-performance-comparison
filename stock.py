b0dfae137a409a9729312b4e2ab934ffed00eb8e
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-09 23:00:36 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -87,7 +87,7 @@ tickers=['TSLA','AAPL','MSFT','BTC-USD','ETH-USD','AMD','AMZN']
 dropdown=st.multiselect('Pick your assets',tickers,key=5,default='TSLA')
 start = st.date_input('Start',dt.date(2021,8, 13))
 end = st.date_input('end',dt.date(2022,8, 14))
-df= yf.download(dropdown,start,end)['Adj Close']
+df= yf.download(dropdown,start,end)
 df=df.shift(1, axis = 0)
 Start = 5000
 df['Shares'] = 0
