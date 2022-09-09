add06750d709b93f79cc4c67f9d81364ec5c2327
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-09 21:10:42 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -262,7 +262,7 @@ if pvalue > 0.05:
     st.write('The returns are likely normal')
 else:
     st.write('The returns are likely not normal.')
-quit()
+return None;
 
   
 st.title('Stock datascience-analysis')
