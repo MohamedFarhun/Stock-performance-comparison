9b042621d73213fb1a001c540d906b4399ecafbd
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-09 20:52:43 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -249,18 +249,18 @@ final_price_as_computed_with_RG=init_price*(1 + R_G)**T
 st.write('final_price_as_computed_with_RG of {} is:-'.format(dropdown),final_price_as_computed_with_RG)
 Harmonic_mean=len(returns)/np.sum(1.0/returns)
 st.write('Harmonic_mean of {} is:-'.format(dropdown),Harmonic_mean)
-st.write('Skew of {} is:-'.format(dropdown),skew)
-st.write('Excess Kurtosis of {} is:-'.format(dropdown),kurtosis)
 skew=stats.skew(returns)
+st.write('Skew of {} is:-'.format(dropdown),skew)
+kurtosis=stats.kurtosis(returns)
+plt1.hist(returns, 30); 
+st.pyplot(plt1)
+st.write('Excess Kurtosis of {} is:-'.format(dropdown),kurtosis)
 xs = np.linspace(-6,6, 1257)
 normal = stats.norm.pdf(xs)
 plt.plot(returns,stats.laplace.pdf(returns), label='Leptokurtic')
 plt.plot(returns,stats.cosine.pdf(returns), label='Platykurtic')
 plt.legend()
 st.pyplot(plt)
-kurtosis=stats.kurtosis(returns)
-plt1.hist(returns, 30); 
-st.pyplot(plt1)
 _, pvalue, _, _ = jarque_bera(returns)
 st.write('The returns of {} :-'.format(dropdown))
 if pvalue > 0.05:
