f6be100fa79ce3412663c577f3aa4d9c41572670
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-28 23:04:25 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -30,19 +30,13 @@ def stock_market():
         if stock_market: return stock_market
 
 with st.sidebar:
+    st.header("STOCK MARKET PREDICTION USING MACHINE LEARNING")
     st.image("stockmarket.jpg")
-    st.header("TEKKYZZ")
-    st.write("Leader   : MOHAMED FARHUN M")
-    st.write("Member 1 : NANDHAKUMAR S")
-    st.write("Member 2 : DHIVAKAR S")
-    st.subheader("**_Do you like our Project_?**")
-    col1, col2 = st.columns([1,1])
-    with col1:
-        if st.button('Yes'):
-            st.write('Thanks for your valuable feedback...')
-    with col2:
-        if st.button('No'):
-            st.write('Will update our app soon...')        
+    st.header("Team name : TEKKYZZ")
+    st.write("Leader     : MOHAMED FARHUN M")
+    st.write("Member 1   : NANDHAKUMAR S")
+    st.write("Member 2   : DHIVAKAR S")
+       
         
         
 def add_bg_from_url():
